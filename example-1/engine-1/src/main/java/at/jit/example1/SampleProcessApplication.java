38a5ad0e28e854c146cbcc1236a567bc4f2deb26
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-03-11 16:53:35 +0100

    Initial commit.

---
+++ example-1/engine-1/src/main/java/at/jit/example1/SampleProcessApplication.java
@@ -0,0 +1,11 @@
+package at.jit.example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProcessApplication {
+    public static void main(String... args) {
+        SpringApplication.run(SampleProcessApplication.class, args);
+    }
+}
